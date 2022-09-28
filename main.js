c2a96d62c19a32d92e0ed4bc6fd33af7fa380faa
hhnssl <losnah5h@gmail.com>
2022-09-28 16:46:39 +0900

    feat - 문서 초기화 및 html, css 퍼블리싱 완료 (footer 제외)

---
+++ main.js
@@ -0,0 +1,13 @@
+// const main = document.getElementById('main');
+
+// fetch('./assets/datas.json') //
+//   .then((res) => res.json())
+//   .then((movie) => {
+//     movie.map((e) => {
+//       const imgEl = document.createElement('img');
+
+//       console.log(e.poster);
+//       imgEl.setAttribute('src', `${e.poster}`);
+//       // main.appendChild(imgEl);
+//     });
+//   });
