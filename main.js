2694ad1d3ec8c02dc149d6ac6e7a3b29168eac9e
hhnssl <losnah5h@gmail.com>
2022-09-29 16:56:51 +0900

    feat - json 수정, JS 영화 정보 로드 기능 구현

--- main.js
+++ main.js
@@ -1,13 +1,47 @@
-// const main = document.getElementById('main');
+const section = document.getElementById('section');
+const movieListUl = document.getElementById('movie__list');
 
-// fetch('./assets/datas.json') //
-//   .then((res) => res.json())
-//   .then((movie) => {
-//     movie.map((e) => {
-//       const imgEl = document.createElement('img');
+function loadItems() {
+  return fetch('/assets/datas.json') //
+    .then((res) => res.json())
+    .then((json) => displayItems(json));
+}
 
-//       console.log(e.poster);
-//       imgEl.setAttribute('src', `${e.poster}`);
-//       // main.appendChild(imgEl);
-//     });
-//   });
+function displayItems(movies) {
+  movies.map((movie) => {
+    const movieItem = `
+  <li class="moive__item">
+    <div class="movie__container">
+        <img class="movie__poster" src="${movie.poster}" alt="">
+        <strong class="movie__title">${movie.title}</strong>
+        <ul class="movie__details">
+          <li class="details__key">
+            개요
+            <span class="details__value">${movie.overview}</span>
+          </li>
+          <li class="details__key">
+            평점
+            <span class="rating-img details__value">${movie.rate}</span>
+          </li>
+          <li class="details__key">
+            개봉
+            <span class="details__value">${movie.release}</span>
+          </li>
+          <li class="details__key">
+            출연
+            <span class="details__value">${movie.cast}</span>
+          </li>
+        </ul>
+    </div>
+    <div class="movie__btn">
+      <button class="movie__booking-btn">예매하기</button>
+      <button class="movie__preview_btn">예고편</button>
+    </div>
+  </li>
+    `;
+
+    movieListUl.insertAdjacentHTML('beforeend', movieItem);
+  });
+}
+
+loadItems();
